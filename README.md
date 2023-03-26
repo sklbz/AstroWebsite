305db03c22bbc287894667110123fffe1e70d914
sklbz <sokolbozanic@gmail.com>
2023-03-26 20:39:10 +0200

    added a server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AstroWebsite
 website with astro.js
+<a href="https://astro-website-f2fd9.web.app/">deployed</a> with firebase
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com/',
+  site: 'https://sklbz.net/',
 });
