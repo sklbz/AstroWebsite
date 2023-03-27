4cf2ef946b89f0a364488ec92f800abb33e074b8
sklbz <sokolbozanic@gmail.com>
2023-03-27 19:53:54 +0200

    started the navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<script></script>
+
+<nav class="navbar">
+	<ul class="navbar-nav">
+		<li class="nav-item">
+			<a href="/" class="navlink">
+				<span class="link-text"></span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a href="/" class="navlink">
+				<span class="link-text"></span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a href="/" class="navlink">
+				<span class="link-text"></span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a href="/" class="navlink">
+				<span class="link-text"></span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a href="/" class="navlink">
+				<span class="link-text"></span>
+			</a>
+		</li>
+	</ul>
+</nav>
+
+<style></style>
