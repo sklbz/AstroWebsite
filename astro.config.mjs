7211107347301714032cb7591174f64cf24869c0
sklbz <sokolbozanic@gmail.com>
2023-03-27 19:40:14 +0200

    svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://sklbz.net/',
-	integrations: [svelte()],
+  integrations: [svelte()]
 });
