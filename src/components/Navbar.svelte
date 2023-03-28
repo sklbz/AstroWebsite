f9a8942d0a93f136b75c3a28d69465047edef965
sklbz <sokolbozanic@gmail.com>
2023-03-28 09:54:13 +0200

    navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,8 +3,31 @@
 <nav class="navbar">
 	<ul class="navbar-nav">
 		<li class="logo">
-			<a href="/" class="navlink logo-text">
-				<span class="link-text" />
+			<a href="/" class="nav-link">
+				<span class="link-text logo-text">SKLBZ</span>
+				<svg
+					aria-hidden="true"
+					focusable="false"
+					data-prefix="fad"
+					data-icon="angle-double-right"
+					role="img"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 448 512"
+					class="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
+				>
+					<g class="fa-group">
+						<path
+							fill="currentColor"
+							d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+							class="fa-secondary"
+						></path>
+						<path
+							fill="currentColor"
+							d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+							class="fa-primary"
+						></path>
+					</g>
+				</svg>
 			</a>
 		</li>
 		<li class="nav-item">
@@ -32,17 +55,18 @@
 
 <style>
 	* {
+		text-decoration: none;
 		list-style-type: none;
 		list-style: none;
-		padding: none;
-		border: none;
-		outline: none;
-		margin: none;
+		padding: 0;
+		border: 0;
+		outline: 0;
+		margin: 0;
 		color: currentColor;
 	}
 	:root {
 		font-size: 16px;
-		font-family: "Open Sans";
+		/* 		font-family: "Open Sans"; */
 		--text-primary: #b6b6b6;
 		--text-secondary: #ececec;
 		--bg-primary: #23232e;
@@ -58,6 +82,9 @@
 	}
 
 	.navbar-nav {
+		list-style: none;
+		padding: 0;
+		margin: 0;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -99,6 +126,19 @@
 		margin: 0 1.5rem;
 	}
 
+	.fa-primary {
+		color: #ff7eee;
+	}
+
+	.fa-secondary {
+		color: #df49a6;
+	}
+
+	.fa-primary,
+	.fa-secondary {
+		transition: var(--transition-speed);
+	}
+
 	.logo {
 		font-weight: bold;
 		text-transform: uppercase;
@@ -111,11 +151,20 @@
 		width: 100%;
 	}
 
+	.logo svg {
+		transform: rotate(0deg);
+		transition: var(--transition-speed);
+	}
+
 	.logo-text {
 		display: inline;
 		position: absolute;
 		left: -999px;
 		transition: var(--transition-speed);
+	}
+
+	.navbar:hover .logo svg {
+		transform: rotate(-180deg);
 	}
 
 	/* Small screens */
@@ -137,13 +186,16 @@
 		.nav-link {
 			justify-content: center;
 		}
+
+		main {
+			margin: 0;
+		}
 	}
 
 	/* Large screens */
 	@media only screen and (min-width: 600px) {
 		.navbar {
 			top: 0;
-			left: 0;
 			width: 5rem;
 			height: 100vh;
 		}
@@ -163,5 +215,42 @@
 		.navbar:hover .logo-text {
 			left: 0px;
 		}
+	}
+
+	.dark {
+		--text-primary: #b6b6b6;
+		--text-secondary: #ececec;
+		--bg-primary: #23232e;
+		--bg-secondary: #141418;
+	}
+
+	.light {
+		--text-primary: #1f1f1f;
+		--text-secondary: #000000;
+		--bg-primary: #ffffff;
+		--bg-secondary: #e4e4e4;
+	}
+
+	.solar {
+		--text-primary: #576e75;
+		--text-secondary: #35535c;
+		--bg-primary: #fdf6e3;
+		--bg-secondary: #f5e5b8;
+	}
+
+	.theme-icon {
+		display: none;
+	}
+
+	.dark #darkIcon {
+		display: block;
+	}
+
+	.light #lightIcon {
+		display: block;
+	}
+
+	.solar #solarIcon {
+		display: block;
 	}
 </style>
