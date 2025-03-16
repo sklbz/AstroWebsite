82825344a6d9d6982e7b83fd4dbef631048ea921
sklbz <sokolbozanic@gmail.com>
2025-03-16 15:40:18 +0100

    update

--- src/components/FancyLogo.svelte
+++ src/components/FancyLogo.svelte
@@ -3,39 +3,35 @@
 	import { fade, draw, fly } from 'svelte/transition';
 	import { expand } from '@js/custom-transition.js';
 	import { inner, outer } from '@js/shape.js';
-  
-	let visible = true;
+	import IntersectionObserver from './IntersectionObserver.svelte'; 
 </script>
 
-{#if visible}
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
-		<g out:fade="{{duration: 200}}" opacity=0.2>
-			<path
-				in:expand="{{duration: 400, delay: 1000, easing: quintOut}}"
-				style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
-				d={outer}
-			/>
-			<path
-				in:draw="{{duration: 1000}}"
-				style="stroke:#ff3e00; stroke-width: 1.5"
-				d={inner}
-			/>
-		</g>
-	</svg>
+<IntersectionObserver let:visible>
+	{#if visible}
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
+			<g out:fade="{{duration: 200}}" opacity=0.2>
+				<path
+					in:expand="{{duration: 400, delay: 1000, easing: quintOut}}"
+					style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
+					d={outer}
+				/>
+				<path
+					in:draw="{{duration: 1000}}"
+					style="stroke:#ff3e00; stroke-width: 1.5"
+					d={inner}
+				/>
+			</g>
+		</svg>
 
-	<div class="centered" out:fly="{{y: -20, duration: 800}}">
-		{#each 'SVELTE' as char, i}
-			<span
-				in:fade="{{delay: 1000 + i * 150, duration: 800}}"
-			>{char}</span>
-		{/each}
-	</div>
-{/if}
-
-<label>
-	<input type="checkbox" bind:checked={visible}>
-	toggle me
-</label>
+		<div class="centered" out:fly="{{y: -20, duration: 800}}">
+			{#each 'SKLBZ' as char, i}
+				<span
+					in:fade="{{delay: 1000 + i * 150, duration: 800}}"
+				>{char}</span>
+			{/each}
+		</div>
+	{/if}
+</IntersectionObserver>
 
 <style>
 	@import url("https://fonts.googleapis.com/css?family=Overpass:100,400&display=swap");
